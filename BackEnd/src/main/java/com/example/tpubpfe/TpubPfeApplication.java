17e0783f5b5c5e010419430e039e302317838de3
khairihleli <khairidinne.hleli@esprit.tn>
2026-07-01 12:33:05 +0100

    Initialize GitHub monorepo

---
+++ BackEnd/src/main/java/com/example/tpubpfe/TpubPfeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpubpfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpubPfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpubPfeApplication.class, args);
+    }
+
+}
